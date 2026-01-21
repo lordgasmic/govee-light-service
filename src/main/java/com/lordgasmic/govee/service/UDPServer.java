a9d281f5645a32adbc3678001c301880c75ff8b5
Nick Wellman <wellman.nicholas@gmail.com>
2026-01-21 01:09:57 -0500

    add more server udp stuff

--- src/main/java/com/lordgasmic/govee/service/UDPServer.java
+++ src/main/java/com/lordgasmic/govee/service/UDPServer.java
@@ -3,10 +3,7 @@ package com.lordgasmic.govee.service;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
+import java.net.*;
 
 @Slf4j
 public class UDPServer implements Runnable {
@@ -17,12 +14,14 @@ public class UDPServer implements Runnable {
     public UDPServer(final int port) {
         try {
             // Bind the socket to the specified port
-//            final InetAddress localhost = InetAddress.getByName("172.16.0.131");
+            final InetAddress localhost = InetAddress.getByName("0.0.0.0");
             socket = new DatagramSocket(port);
             log.info("UDP Server started on ip: {}", socket.getInetAddress().getHostAddress());
 //            log.info("UDP Server started on host: {}", socket.getInetAddress().getHostName());
             log.info("UDP Server started on port {}", port);
         } catch (final SocketException e) {
+            throw new RuntimeException(e);
+        } catch (final UnknownHostException e) {
             throw new RuntimeException(e);
         }
     }
